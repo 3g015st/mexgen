b6b93420dd8f08607dabdf5656d339d4269792ab
Benedict Lutab <markbenedict98@gmail.com>
2020-03-11 12:11:26 +0800

    Fixed working directory path

--- references/file-template-path-lookup.js
+++ references/file-template-path-lookup.js
@@ -1,8 +1,8 @@
-const PATH_PREFIX = './references/';
+const PATH_PREFIX = __dirname;
 
 module.exports = {
-  'Model': PATH_PREFIX + 'model-template.txt',
-  'Route': PATH_PREFIX + 'route-template.txt',
-  'Action': PATH_PREFIX + 'action-template.txt',
-  'Controller': PATH_PREFIX + 'controller-template.txt'
+  'Model': PATH_PREFIX + '\\model-template.txt',
+  'Route': PATH_PREFIX + '\\route-template.txt',
+  'Action': PATH_PREFIX + '\\action-template.txt',
+  'Controller': PATH_PREFIX + '\\controller-template.txt'
 };
